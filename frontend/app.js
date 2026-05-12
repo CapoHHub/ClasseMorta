cd99013f8f81b8f27f2471916f85ee478967b624
Riccardo <rikigamer160@gmail.com>
2026-05-12 19:12:42 +0200

    fix app.js

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080"
-    : "https://classemorta-production.up.railway.app/";
+    : "https://classemorta-production.up.railway.app/api";
 
 window.API_URL = API_URL;
 
